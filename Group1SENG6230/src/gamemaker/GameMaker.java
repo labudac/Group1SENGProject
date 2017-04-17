16c026990265d8965d7010467607531fdb5e07ba
labudac <labudac17@students.ecu.edu>
2017-04-17 17:25:32 -0400

    Signed-off-by: labudac <labudac17@students.ecu.edu>

---
+++ Group1SENG6230/src/gamemaker/GameMaker.java
@@ -0,0 +1,13 @@
+package gamemaker;
+
+
+/*
+ * Class for GameMaker GUI
+ * @author Group 1
+ */
+public class GameMaker {
+
+	public static void main(String[] args) {
+        System.out.println("Welcome to the Game Maker.");
+	}
+}
---
+++ Group1SENG6230/src/spellergame/SinglevsGroup.java
@@ -0,0 +1,5 @@
+package spellergame;
+
+public class SinglevsGroup {
+
+}
